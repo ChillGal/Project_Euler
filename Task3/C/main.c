69d77fe609c6a6d3ce825188e260b91516305f70
Jennifer Chan <ed18gfc@leeds.ac.uk>
2021-06-08 05:43:58 +0100

    Task3 Solution - INEFFICIENT

--- Task3/C/main.c
+++ Task3/C/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <math.h>
+int primeFactor(long long num){
+    long long pfactor = 0;
+    //Loop from 0 to the number
+    for (long long i = 0; i < num; i++){
+        if (fmodf(i,num) == 0){
+            if ( i > pfactor){
+                pfactor = i;
+            }
+        }
+    }
+    return pfactor;
+
+}
 
 int main(){
-    printf("The largest prime factor of the number 600851475143 = ")
+    printf("The largest prime factor of the number 600851475143 = %i",primeFactor(600851475143));
     return 0;
 }
